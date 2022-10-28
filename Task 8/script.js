91a9c28ea92b213b4dad435fdefca128eb6fb500
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-28 17:54:33 +0300

    JS Exam task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function calculator(firstNumber, secondNumber, operator) {
+    operator = prompt("Koks veiksmas: sum, subtraction, multiplication, division?", "sum")
+    firstNumber = +prompt("Enter first number", 2);
+    secondNumber = +prompt("Enter second number", 2);
+
+    if (operator === "sum") {
+        return firstNumber + secondNumber;
+
+    } else if (operator === "subtraction") {
+        return firstNumber - secondNumber;
+
+    } else if (operator === "multiplication") {
+        return firstNumber * secondNumber;
+
+    } else if (operator === "division") {
+        return firstNumber / secondNumber;
+
+    } else { }
+}
+
+alert(calculator("", "", "operator"));
+
+
+
