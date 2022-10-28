20c8df0c7b26df9bb66ba4e17abd23ffb65ca81b
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-28 15:12:50 +0300

    JS Exam task 9

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,16 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+function Movie(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+    this.isBudgetWasExpensive = favoriteMovieBudget > moreThanMovieBudget ? alert("true") : alert("false");
+};
+
+const moreThanMovieBudget = 100_000_000;
+const favoriteMovieBudget = +prompt("Enter Your favorite movie budget, $", 120_000_000);
+const favoriteMovieTitle = prompt("Enter Your favorite movie title", "Titanik");
+const favoriteMovieDirector = prompt("Enter Your favorite movie director", "James Cameron");
+const favorietMovie = new Movie(favoriteMovieTitle, favoriteMovieDirector, favoriteMovieBudget);
