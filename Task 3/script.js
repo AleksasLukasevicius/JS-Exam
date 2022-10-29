4afa361340535ee7476fd8fe4c53d1dd35392157
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-29 21:06:59 +0300

    Task 2 inprogress

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,3 +12,37 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+const getUsers = async () => {
+    try {
+        const response = await fetch(ENDPOINT);
+        const users = await response.json();
+
+        console.info({ users })
+
+        return users;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getUsers();
+
+// const createUserCard = (user) => {
+//     const img = document.createElement('img');
+//     img.src = user.picture.large;
+//     img.alt = `${user.name.first} profile picture`;
+
+//     const userFullName = document.createElement('h2');
+//     userFullName.innerText = `${user.name.first} ${user.name.last}`;
+
+//     const userAge = document.createElement('h4');
+//     userAge.innerText = `${user.dob.age} years old`;
+
+//     const contacts = document.createElement('h4');
+//     contacts.innerText = `E-mail: ${user.email}`;
+
+//     const card = document.createElement('div');
+//     card.prepend(img, userFullName, userAge, contacts);
+//     document.body.div.prepend(card);
+// };
