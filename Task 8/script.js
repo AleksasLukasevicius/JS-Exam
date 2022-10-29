ae18e634831826e0e4da2882e179c6918c2e1c13
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-29 15:54:13 +0300

    Task 8 fix

--- Task 8/script.js
+++ Task 8/script.js
@@ -9,59 +9,31 @@ division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 function Calculator() {
 
     this.getNumbers = function () {
-        this.firstNumber = +prompt('a?', 0);
-        this.secondNumber = +prompt('b?', 0);
+        this.firstNumber = +prompt('Enter first nubmer', 0);
+        this.secondNumber = +prompt('Enter second nubmer', 0);
     };
 
     this.getSum = function () {
         return this.firstNumber + this.secondNumber;
     };
 
-    this.getsubtraction = function () {
-        return this.firstNumber + this.secondNumber;
+    this.getSubtraction = function () {
+        return this.firstNumber - this.secondNumber;
     };
 
     this.getMultiplication = function () {
         return this.firstNumber * this.secondNumber;
     };
 
-    this.getdivision = function () {
-        return this.firstNumber + this.secondNumber;
+    this.getDivision = function () {
+        return this.firstNumber / this.secondNumber;
     };
 }
 
 let calculator = new Calculator();
 calculator.getNumbers();
 
-console.info("Sum=" + calculator.getSum());
-console.info("Mul=" + calculator.getsubtraction());
-console.info("Mul=" + calculator.getMultiplication());
-console.info("Mul=" + calculator.getdivision());
-
-
-
-
-
-
-// const firstNumber = Math.random() * 10;
-// const secondNumber = Math.random() * 5;
-// console.info(firstNumber, secondNumber)
-
-// function Calculator(firstNumber, secondNumber) {
-//     firstNumber;
-//     secondNumber;
-
-//     this.sum = () => {
-//         console.info(firstNumber + secondNumber).toFixed(2);
-//     }
-
-//     this.subtraction = () => {
-//         console.info(firstNumber - secondNumber).toFixed(2);
-//     };
-// }
-
-// // let getSum = new Calculator(firstNumber, secondNumber);
-// // getSum.sum();
-
-// let getsubtraction = new Calculator(firstNumber, secondNumber);
-// getSum.subtraction();
+console.info(`Sum = ${calculator.getSum()}`);
+console.info(`Subtraction =  ${calculator.getSubtraction()}`);
+console.info(`Multiplication = ${calculator.getMultiplication()}`);
+console.info(`Division = ${calculator.getDivision()}`);
