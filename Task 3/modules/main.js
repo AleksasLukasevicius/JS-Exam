84b279383336efa729b4c9ccb505cab48d9fa2a7
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-30 14:24:09 +0200

    Task 3 work progress

--- Task 3/modules/main.js
+++ Task 3/modules/main.js
@@ -1,8 +1,23 @@
-import { getUsers } from "./modules/getusers.js";
+/* ------------------------------ TASK 3 -----------------------------------
+Parašykite JS kodą, kuris leis vartotojui paspaudus ant mygtuko "Show users"
+pamatyti vartotojus iš Github API (endpoint'as pateiktas žemiau).
+
+Paspaudus mygtuką "Show users":
+1. Informacija atvaizdavima <div id="output"></div> bloke
+1.1. Informacija, kuri pateikiama: "login" ir "avatar_url" reikšmės (kortelėje)
+2. Žinutė "Press "Show Users" button to see users" turi išnykti;
+
+Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi 
+turėti bent minimalų stilių ir būti responsive;
+-------------------------------------------------------------------------- */
+
+import { getUsers } from "./modules/getUsers.js";
 
 const handleFormSubmit = async (event) => {
     event.preventDefault();
+
     document.querySelector("p").style.visibility = "hidden";
+
     await getUsers();
 };
 document.querySelector("#button").addEventListener("click", handleFormSubmit);
