51405362641e715597483fe98661a3a5aebef335
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-30 15:46:59 +0200

    Task 3 fixes

--- Task 3/modules/showUsers.js
+++ Task 3/modules/showUsers.js
@@ -2,11 +2,11 @@ const showUsers = (users) => {
     users.forEach((user) => {
         const usersContainerWrapper = document.querySelector("#output");
         const userCard = document.createElement("div");
-        const userCardLoginElement = document.createElement("h4");
         const userCardAvatar = document.createElement("img");
+        const userCardLoginElement = document.createElement("h5");
 
-        userCardLoginElement.innerText = user.login;
         userCardAvatar.src = user.avatar_url;
+        userCardLoginElement.innerText = user.login;
 
         userCard.prepend(userCardAvatar, userCardLoginElement);
         usersContainerWrapper.prepend(userCard);
