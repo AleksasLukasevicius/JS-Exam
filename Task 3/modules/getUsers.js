4b3d14481134f95880e2f74a013880b9240276f8
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2022-10-30 14:13:40 +0200

    Task 1 fix js grammar

--- Task 3/modules/getUsers.js
+++ Task 3/modules/getUsers.js
@@ -26,22 +26,3 @@ const getUsers = async () => {
 };
 
 getUsers();
-
-// const createUserCard = (user) => {
-//     const img = document.createElement('img');
-//     img.src = user.picture.large;
-//     img.alt = `${user.name.first} profile picture`;
-
-//     const userFullName = document.createElement('h2');
-//     userFullName.innerText = `${user.name.first} ${user.name.last}`;
-
-//     const userAge = document.createElement('h4');
-//     userAge.innerText = `${user.dob.age} years old`;
-
-//     const contacts = document.createElement('h4');
-//     contacts.innerText = `E-mail: ${user.email}`;
-
-//     const card = document.createElement('div');
-//     card.prepend(img, userFullName, userAge, contacts);
-//     document.body.div.prepend(card);
-// };
